24156c609738e52723a8611ad5b7318af5ff8982
Pierre Rabaud <pirabaud@1C4.42angouleme.fr>
2022-06-07 15:01:40 +0200

    finis partie obligatoire

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pirabaud <pirabaud@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 09:59:03 by pirabaud          #+#    #+#             */
-/*   Updated: 2022/06/06 16:56:10 by pirabaud         ###   ########.fr       */
+/*   Updated: 2022/06/07 15:00:59 by pirabaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,19 @@ void	free_sons(t_sons *sons)
 	if (sons->cmd != NULL)
 	{	
 		while(sons->cmd[i] != NULL)
-			free(sons->cmd[i++]);
+		{
+			free(sons->cmd[i]);
+			sons->cmd[i++] = NULL;
+		}
 		free(sons->cmd);
+		sons->cmd = NULL;
 	}
 	if (sons->path != NULL)
+	{
 		free(sons->path);
+		sons->path = NULL;
+	}
+	free(sons);
 }
 
 void	print_error(char *str, t_sons *first, t_sons *second)
@@ -40,6 +48,27 @@ void	print_error(char *str, t_sons *first, t_sons *second)
 	exit(1);
 }
 
+void	free_dpointer(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != NULL)
+		free(str[i++]);
+	free(str);
+}
+
+char *init_res(char *path, char *cmd)
+{
+	char *tmp;
+	char *res;
+
+	tmp = ft_strjoin(path, "/");
+	res = ft_strjoin(tmp, cmd);
+	free(tmp);
+	return(res);
+}
+
 char	*check_path(char *cmd, char **env)
 {
 	int	i;
@@ -51,18 +80,18 @@ char	*check_path(char *cmd, char **env)
 		i++;
 	path = ft_split(env[i], ':');
 	i = 0;
-	res = ft_strjoin(path[i], cmd);
+	res = init_res(path[i++], cmd);
 	while (access(res, F_OK) != 0 && path[i] != NULL)
 	{
 		free(res);
-		res = ft_strjoin(path[i], "/");
-		res = ft_strjoin(res, cmd);
-		i++;
+		res = init_res(path[i++], cmd);
 	}
+	free_dpointer(path);
 	if (access(res, F_OK) == 0)
 		return (res);
-	else if (access(cmd, F_OK) == 0)
-		return (cmd);
+	free(res);
+	if (access(cmd, F_OK) == 0)
+		return (ft_strdup(cmd));
 	else
 		return (NULL);
 }
@@ -81,14 +110,14 @@ void	first_call(int *pipexfd, char **env, t_sons *first)
 	execve(first->path, first->cmd, env);
 }
 
-void	second_call(t_sons *second, int *pipexfd, char **env, pid_t first)
+void	second_call(t_sons *second, int *pipexfd, char **env, t_sons *first)
 {
 	int fd;
 
 	fd = open(second->file,O_WRONLY | O_TRUNC | O_CREAT, S_IRWXU);
 	if (fd == -1)
 		print_error(strerror(errno), first, second);	
-	waitpid(first, NULL, 0);
+	waitpid(first->son, NULL, 0);
 	close(pipexfd[1]);
 	dup2(fd, 1);
 	dup2(pipexfd[0], 0);
@@ -96,14 +125,29 @@ void	second_call(t_sons *second, int *pipexfd, char **env, pid_t first)
 	execve(second->path, second->cmd, env);
 }
 
+t_sons	*init_null(void)
+{
+	t_sons *init;
 
-t_sons	*init_first(char **argv, char **env)
+	init = malloc(sizeof(t_sons));
+	if (init == NULL)
+	{
+		perror(strerror(errno));
+		exit (1);
+	}
+	init->file = NULL;
+	init->cmd = NULL;
+	init->path = NULL;
+	
+	return (init);
+}
+
+
+t_sons	*init_first(char **argv, char **env, t_sons *init)
 {
 	t_sons *first;
 	
-	first = malloc(sizeof(t_sons));
-	if (first == NULL)
-		print_error(strerror(errno), first, NULL);
+	first = init;
 	first->file = ft_strdup(argv[1]);
 	if (first->file == NULL)
 		print_error(strerror(errno), first, NULL);
@@ -119,25 +163,23 @@ t_sons	*init_first(char **argv, char **env)
 	return (first);
 }
 
-t_sons	*init_second(char **argv, char **env)
+t_sons	*init_second(char **argv, char **env, t_sons *init, t_sons *first)
 {
 	t_sons *second;
 	
-	second = malloc(sizeof(t_sons));
-	if (second == NULL)
-		print_error(strerror(errno), second, NULL);
+	second = init;
 	second->file = ft_strdup(argv[4]);	
 	if (second->file == NULL)
-		print_error(strerror(errno), second, NULL);
+		print_error(strerror(errno), second, first);
 	second->cmd	= ft_split(argv[3], ' ');
 	if (second->cmd == NULL)
-		print_error(strerror(errno), second, NULL);
+		print_error(strerror(errno), second, first);
 	second->path = check_path(second->cmd[0], env);
 	if (second->path == NULL)
-		print_error(strerror(errno), second, NULL);
+		print_error(strerror(errno), second, first);
 	second->son = fork();
 	if (second->son == -1)
-		print_error(strerror(errno), second, NULL);
+		print_error(strerror(errno), second, first);
 	return (second);
 }
 
@@ -153,12 +195,14 @@ int	main(int argc, char **argv, char **env)
 		return(0);
 	}
 	pipe(pipexfd);
-	first = init_first(argv, env);
+	first =	init_null();
+	first = init_first(argv, env, first);
 	if (first->son == 0)
 		first_call(pipexfd, env, first);
-	second = init_second(argv, env);
+	second = init_null();
+	second = init_second(argv, env, second, first);
 	if (second->son == 0)
-		second_call(second, pipexfd, env, first->son);
+		second_call(second, pipexfd, env, first);
 	close(pipexfd[1]);
 	close(pipexfd[0]);
 	waitpid(first->son, NULL, 0);
